7a227c57f76e15c33e904eab048c20a7053187ea
Awaludin261999 <a704ybm259@devacademy.id>
2025-06-11 02:30:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -232,3 +232,8 @@ def form():
     return render_template('form.html', instansi=instansi_df, 
                            kecamatan=kecamatan_df, kelurahan=kelurahan_df, 
                            topik=topik_df, kelurahan_map=json.dumps(kelurahan_map), message=message)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=port)
+
